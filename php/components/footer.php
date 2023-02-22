82b64794c2ba6aad6a3e4daeab00380c4aaf6757
BlendOsmanii <121260874+BlendOsmanii@users.noreply.github.com>
2023-02-22 15:46:17 +0000

    shtimi i footerit

---
+++ php/components/footer.php
@@ -0,0 +1,42 @@
+<footer class="footer">
+    <div class="kontenjeri">
+      <div class="rreshti">
+        <div class="footer-col">
+          <h4>Kompania</h4>
+          <ul>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Sherbimet tona</a></li>
+            <li><a href="#">Politikat e privatesise</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Ju nevojitet ndihme?</h4>
+          <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Posta</a></li>
+            <li><a href="#">Kthimet</a></li>
+            <li><a href="#">Statusi i porosise</a></li>
+            <li><a href="#">Opsionet e pageses</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Produktet</h4>
+          <ul>
+            <li><a href="#">Kartela Grafike</a></li>
+            <li><a href="#">Kompjutere</a></li>
+            <li><a href="#">Monitora</a></li>
+            <li><a href="#">Aksesore</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Na ndiqni</h4>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+ </footer>
