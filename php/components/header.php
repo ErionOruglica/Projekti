235dbfabf5121ae012bdc3b008b60bd07db6de6f
ErionOruglica <eo61125@ubt-uni.net>
2023-02-25 15:44:13 +0100

    ndryshime,debugging

---
+++ php/components/header.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="views/php/css/nav.css"/>
+    <title>Responsive Animated Navbar</title>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <img src="/php/Fotot/logo.png" alt="Logo Image">
+        </div>
+        <div class="hamburger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Solutions</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><button class="login-button" href="#">Login</button></li>
+            <li><button class="join-button" href="#">Join</button></li>
+        </ul>
+    </nav>
+    <script src="scripts/nav.js"></script>
--- php/views/contactForm.php
+++ php/views/contactForm.php
@@ -1,10 +1,4 @@
-<?php
-require_once '../controllers/MenuController.php';
-$menu= new MenuController;
-if(isset($_POST['submit'])){
-    $menu->insertMessages($_POST);
-}
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,11 +6,13 @@ if(isset($_POST['submit'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel ="stylesheet" href="../css/contact.css">
+    <link rel ="stylesheet" href="./css/contact.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-
+<?php
+    include "../components/header.php";
+ ?>
     <section class ="kontakt">
     <div class ="container">
         <h2>Na kontaktoni</h2>
